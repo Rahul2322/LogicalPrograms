5bfdbfc039b4923ec1331af7caa5d2b2f2ea988f
rahul <rahulsingh@nimapinfotech.com>
2023-07-31 09:07:22 +0530

    'logicalPrograms'

---
+++ qr.js
@@ -0,0 +1,12 @@
+const m = new Map();
+m.set('2' , ['a','b','c'])
+m.set('3' , ['d','e','f'])
+m.set('4' , ['g','h','i'])
+m.set('5' , ['j','k','l'])
+m.set('6' , ['m','n','o'])
+m.set('7' , ['p','q','r','s'])
+m.set('8' , ['t','u','v'])
+m.set('9' , ['w','x','y','z'])
+
+
+console.log(m.get('2'))
